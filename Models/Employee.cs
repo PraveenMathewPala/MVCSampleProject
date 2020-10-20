eb7e393c3fedd4d9aac9b570d213a2124b3dabcf
praveen <praveenmathew@qburst.com>
2020-10-20 16:13:29 +0530

    moded employeee

---
+++ Models/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCSampleProject.Models
+{
+    public class Employee
+    {
+        [Required]
+        [Key]
+        public int Empid
+        {
+            get; set;
+        }
+        [Required]
+        public string Empname
+        {
+            get; set;
+        }
+        [Required]
+        public string DeptName
+        {
+            get; set;
+        }
+        [Required]
+        public long PhoneNo
+        {
+            get; set;
+        }
+        [Required]
+        public string password
+        {
+            get; set;
+        }
+        [Required]
+        public string confirmPassword
+        {
+            get; set;
+        }
+    }
+}
---
+++ Models/EmployeeLogin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCSampleProject.Models
+{
+    public class EmployeeLogin
+    {
+        [Required]
+        [Key]
+        public int userid
+        {
+            get;set;
+        }
+        [Required]
+        public string password
+        {
+            get;set;
+        }
+        [Required]
+        public string confirmPassword
+        {
+            get;set;
+        }
+    }
+}
